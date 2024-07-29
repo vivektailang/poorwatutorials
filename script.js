068652e5f1a2410e1f3459b66f2d7cd26ce5e832
vivektailang <vivek_tailang@hotmail.com>
2024-07-29 14:33:29 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const navToggle = document.getElementById('navToggle');
+    const navMenu = document.getElementById('navMenu');
+
+    navToggle.addEventListener('click', function() {
+        navMenu.classList.toggle('show');
+    });
+});
